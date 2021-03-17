4462a2560b750597a615a32b9ea754b190c52a38
Erdem <57351007+ErdemGns@users.noreply.github.com>
2021-03-18 02:01:41 +0300

    Update 003.dart

--- 003.dart
+++ 003.dart
@@ -4,5 +4,11 @@
  */
 
 void main() {
-  print("Hello world!");
+  // Hello World!
+  print("Hello World!");
+  // Sum 2 + 3
+  int number1 = 2;
+  int number2 = 3;
+  int total = number1 + number2;
+  print(total);
 }
