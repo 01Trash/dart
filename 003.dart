8450f740361d0638e3ffa0234f74afdb35df8164
Erdem <57351007+ErdemGns@users.noreply.github.com>
2021-03-18 02:06:13 +0300

    Update 003.dart

--- 003.dart
+++ 003.dart
@@ -10,4 +10,14 @@ void main() {
   int number2 = 3;
   int total = number1 + number2;
   print(total);
+  // if, else if, else
+  int a = 34;
+  int b = 25;
+  if (a > b) {
+    print("a > b");
+  } else if (a < b) {
+    print("a < b");
+  } else {
+    print("a = b");
+  }
 }
